3cb1904f3d2b7c2abdb4c6c92b6d1ffd1ac2dc7c
root <zhaoyafei0210@gmail.com>
2019-02-01 23:17:06 +0000

    update some *.ipynb in examples

--- unfinished-layers/include/heatmap2coord_layer.hpp
+++ unfinished-layers/include/heatmap2coord_layer.hpp
@@ -25,25 +25,24 @@ template <typename Dtype>
 class Heatmap2CoordLayer : public Layer<Dtype>
 {
   public:
-    explicit Heatmap2CoordLayer(const LayerParameter &param)
+    explicit Heatmap2CoordLayer(const LayerParameter & param)
         : Layer<Dtype>(param) {}
-    virtual void LayerSetUp(const vector<Blob<Dtype> *> &bottom,
-                            const vector<Blob<Dtype> *> &top);
-    virtual void Reshape(const vector<Blob<Dtype> *> &bottom,
-                         const vector<Blob<Dtype> *> &top);
+    virtual void LayerSetUp(const vector<Blob<Dtype> *> & bottom, const vector<Blob<Dtype> *> & top);
+    virtual void Reshape(const vector<Blob<Dtype> *> & bottom,
+                         const vector<Blob<Dtype> *> & top);
 
     virtual inline const char *type() const { return "Heatmap2coord"; }
     virtual inline int ExactNumBottomBlobs() const { return 1; }
     virtual inline int ExactNumTopBlobs() const { return 1; }
 
   protected:
-    virtual void Forward_cpu(const vector<Blob<Dtype> *> &bottom,
-                             const vector<Blob<Dtype> *> &top);
-    virtual void Forward_gpu(const vector<Blob<Dtype> *> &bottom,
-                             const vector<Blob<Dtype> *> &top);
-    virtual void Backward_cpu(const vector<Blob<Dtype> *> &top,
-                              const vector<bool> &propagate_down,
-                              const vector<Blob<Dtype> *> &bottom)
+    virtual void Forward_cpu(const vector<Blob<Dtype> *> & bottom,
+                             const vector<Blob<Dtype> *> & top);
+    virtual void Forward_gpu(const vector<Blob<Dtype> *> & bottom,
+                             const vector<Blob<Dtype> *> & top);
+    virtual void Backward_cpu(const vector<Blob<Dtype> *> & top,
+                              const vector<bool> & propagate_down,
+                              const vector<Blob<Dtype> *> & bottom)
     {
         NOT_IMPLEMENTED;
     }
